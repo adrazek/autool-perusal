a05771f5940a3a9e9ba0237ecaf888267eb67208
nO <adrazek@gmail.com>
2015-06-07 14:08:45 +0200

    Managing engine configuration

--- lib/autool_perusal/engine.rb
+++ lib/autool_perusal/engine.rb
@@ -1,7 +1,12 @@
 module AutoolPerusal
   class Engine < ::Rails::Engine
     isolate_namespace AutoolPerusal
-    initializer("autool_notebook.locales") do |app|
+    
+    config.to_prepare do
+      #BaseToolEngine.new.initialize_engineinitalize_engine
+    end
+
+    initializer("autool_perusal.locales") do |app|
       tracking_logger = Logger.new(app.root.join('log', "my_engine_log.log"), 10, 30*1024*1024)
       AutoolNotebook::Engine.config.i18n.load_path += Dir[root.join('config', 'locales', '*.{rb,yml}').to_s]
       AutoolNotebook::Engine.config.i18n.default_locale = :fr
@@ -9,6 +14,13 @@ module AutoolPerusal
       tracking_logger.debug "AutoolPerusal::Engine specific locale settings are set. Def locale == :fr\n\n"
     end
 
+    initializer("autool_perusal.migrations") do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
 
   end
 end
