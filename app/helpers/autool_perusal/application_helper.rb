43100954b55de265a4c6a74c1e59200c101877bb
Arnaud DRAZEK <adrazek@gmail.com>
2015-06-10 17:55:41 +0200

    Refactoring : passing common code in autool

--- app/helpers/autool_perusal/application_helper.rb
+++ app/helpers/autool_perusal/application_helper.rb
@@ -1,14 +1,5 @@
 module AutoolPerusal
   module ApplicationHelper
-
-    # Allox the engine to get the parent application defined route paths
-    def method_missing(method, *args, &block)
-      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
-        main_app.send(method, *args)
-      else
-        super
-      end
-    end
-
+    include ToolBaseApplicationHelper 
   end
 end
